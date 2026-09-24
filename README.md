1614bee3ec23995a01a2d58b9d2a8f58f60b2909
Xitiz Magar <xitizmgr1234567890@gmail.com>
2026-09-25 01:13:01 +0545

    update the readme and fixied the door opening prb

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # What is haunted door ?
-> it's really a single scarry looking door only . heh heh :>
+> it's really a single scarry looking door only which u shouldn;t open but obviously you will :) heh heh :>
+The idea of haunted door  is pretty simple. You enter a dark hallway and see a creepy old door. The clock starts ticking at 11:59:45 PM, and you have 15 seconds before midnight.
+At 12:00 AM, things change.
+
+A flashlight appears, the door becomes interactable, and well... you know what happens next. (hehe can't tell yayy u have to see for your self  you have to wait and see after the door open to see the suprise)
+
+# Feature 
+> it doesn't that many feature but below are some which u will see in it
+- interactive door
+- mouse-controlled flashlight
+- volume controler 
+> other are not worth mentioning TwT..
+
+# Tech Stack
+- Html 5
+- css
+- JavaScript
+- vite
+
+# Running the project
+
+To run the project there are two ways :
+- Either u can visit the live website here: **https://haunted-door.vercel.app/ **
+- Or u can Clone the repository and run it locally (npm run dev) .
--- src/main.js
+++ src/main.js
@@ -19,70 +19,70 @@ backgroundAudio.volume = 0.5;
 clockAudio.volume = 0.5;
 let audioStarted = false;
 function startBackgroundAudio() {
-  if (audioStarted) return;
-  backgroundAudio.play();
-  audioStarted = true;
+    if (audioStarted) return;
+    backgroundAudio.play();
+    audioStarted = true;
 }
 let seconds = 0;
 let clockStarted = false;
 function startClock() {
-  if (clockStarted) return;
-  clockStarted = true;
-  clockAudio.currentTime = 0;
-  clockAudio.play();
-  const clockInterval = setInterval(() => {
-    seconds++;
-    if (seconds < 15) {
-      time.textContent =
-        `11:59:${String(45 + seconds).padStart(2, "0")} PM`;
-    } else {
-      clearInterval(clockInterval);
-      time.textContent = "12:00 AM";
-      midnight();
-    }
-  }, 1000);
+    if (clockStarted) return;
+    clockStarted = true;
+    clockAudio.currentTime = 0;
+    clockAudio.play();
+    const clockInterval = setInterval(() => {
+        seconds++;
+        if (seconds < 15) {
+            time.textContent =
+                `11:59:${String(45 + seconds).padStart(2, "0")} PM`;
+        } else {
+            clearInterval(clockInterval);
+            time.textContent = "12:00 AM";
+            midnight();
+        }
+    }, 1000);
 }
-
 function midnight() {
-  clockAudio.pause();
-  flashlightEnabled = true
-  flashlight.classList.add("active");
-  flashlight.style.setProperty( "--mouse-x", `${window.innerWidth / 2}px` );
-  flashlight.style.setProperty( "--mouse-y",`${window.innerHeight / 2}px`);
-  backgroundAudio.pause();
+    clockAudio.pause();
+    flashlightEnabled = true;
+    flashlight.classList.add("active");
+    flashlight.style.setProperty("--mouse-x", `${window.innerWidth / 2}px`);
+    flashlight.style.setProperty("--mouse-y", `${window.innerHeight / 2}px`
+    );
+    backgroundAudio.pause();
+    openButton.textContent = "OPEN THE DOOR";
 }
-
 let doorOpened = false;
 function openDoor() {
-  if (doorOpened) return;
-  doorOpened = true;
-  door.classList.add("open");
-  doorAudio.currentTime = 0;
-  doorAudio.play();
-  jumpscare.classList.add("active");
+    if (!flashlightEnabled) return;
+    if (doorOpened) return;
+    doorOpened = true;
+    door.classList.add("open");
+    doorAudio.currentTime = 0;
+    doorAudio.play();
+    setTimeout(() => {
+        jumpscare.classList.add("active");
+    }, 15000);
 }
+
 door.addEventListener("click", openDoor);
 openButton.addEventListener("click", openDoor);
 document.addEventListener("click", () => {
-  startBackgroundAudio();
-
-});
-
+    startBackgroundAudio();
+    clockAudio.play();
+}, { once: true });
 audioController.addEventListener("input", () => {
-  const volume = Number(audioController.value);
-  backgroundAudio.volume = volume / 100;
-  clockAudio.volume = volume / 100;
-  doorAudio.volume = volume / 100;
-  audioValue.textContent = `${volume}%`;
-
+    const volume = Number(audioController.value);
+    backgroundAudio.volume = volume / 100;
+    clockAudio.volume = volume / 100;
+    doorAudio.volume = volume / 100;
+    audioValue.textContent = `${volume}%`;
 });
 document.addEventListener("mousemove", (event) => {
-  if (!flashlightEnabled) return;
-
-  flashlight.style.setProperty("--mouse-x", `${event.clientX}px` );
- flashlight.style.setProperty("--mouse-y", `${event.clientY}px` );
-console.log(event.clientX,event.clientY)
+    if (!flashlightEnabled) return;
+    flashlight.style.setProperty("--mouse-x", `${event.clientX}px`);
+    flashlight.style.setProperty("--mouse-y", `${event.clientY}px`);
+    console.log(event.clientX, event.clientY);
 });
-
 let flashlightEnabled = false;
 startClock();
