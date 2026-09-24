81b5bf63e73318e072a6832b2a90bcb85ae7d9ed
Xitiz Magar <xitizmgr1234567890@gmail.com>
2026-09-24 19:09:39 +0545

    added some audio

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+const door =  document.querySelector(".door")
+const doorAudio = document.getElementById("door-audio")
+
+doorAudio.volume = 0.5
+door.addEventListener("click",()=>{
+   door.classList.toggle("open")
+   doorAudio.play()
+})
+
+const backgroundAudio = document.querySelector("#background-audio")
+
+
+backgroundAudio.volume = 0.5
+document.addEventListener("click",()=>{
+    backgroundAudio.play()
+})
